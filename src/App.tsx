31009d73542a24f9d676d27bbb785d8ec68d200b
wethigha27 <git config --global user.email weythyghasidimohamed@gmail.com>
2024-12-05 18:23:32 +0000

    k

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { GameTcha } from './components/game-tcha/GameTcha';
 import Register from './App_Register_page'
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-      <div className="w-full max-w-2xl">
+    // <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+    //   <div className="w-full max-w-2xl">
         <Register />
-      </div>
-    </div>
+    //   </div>
+    // </div>
   );
 }
 
